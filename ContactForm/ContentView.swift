e7993a9f713f5cd96f1db23e17cf04aee8aa615f
laetuz <laetuzg@gmail.com>
2023-11-04 01:21:37 +0700

    feat: added sections, picker, toggle, and button for references.

--- ContactForm/ContentView.swift
+++ ContactForm/ContentView.swift
@@ -8,12 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var name = ""
+    @State private var subject = "Help"
+    @State private var message = "Write message here..."
+    @State private var includeLogs = false
+    
     var body: some View {
         Form {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Section("Intro"){
+                HStack{
+                    Image(systemName: "globe")
+                        .imageScale(.large)
+                        .foregroundStyle(.tint)
+                    Text("Hello, world!")
+                }
+            }
+            Section("What is your name?"){
+                TextField("Name", text: $name)
+            }
+            Section("What can we help you with?"){
+                Picker("Subject", selection: $subject){
+                    ForEach(["Help", "Suggestion", "Bug Report"], id: \.self){
+                        subject in Text(subject)
+                    }
+                }
+            }
+            Section("Briefly explain what's going on"){
+                TextEditor(text: $message)
+            }
+            Section(footer: Text("Shi")){
+                Toggle("Include Logs", isOn: $includeLogs)
+            }
+            
+            Button("Submit") {
+                print("Subit Button Tapped")
+            }
         }.padding()
     }
 }
