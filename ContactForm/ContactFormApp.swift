097596bce2063044aaa2aadb049c7f0c8eecf5aa
laetuz <laetuzg@gmail.com>
2023-11-03 16:50:51 +0700

    Initial Commit

---
+++ ContactForm/ContactFormApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactFormApp.swift
+//  ContactForm
+//
+//  Created by Ryo Martin on 03/11/23.
+//
+
+import SwiftUI
+
+@main
+struct ContactFormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
